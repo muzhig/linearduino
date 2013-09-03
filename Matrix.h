4e24702f783bbe9780f81e74a6de1ab3107e524d
muzhig <homeip127@gmail.com>
2013-09-03 16:13:03 +0600

    Update Matrix.h
    some optimizations + const validness

--- Matrix.h
+++ Matrix.h
@@ -3,33 +3,40 @@
 
 class Matrix {
 public:
-	Matrix(int m, int n);
+	Matrix(int m=0, int n=0, double* data=0);
 	Matrix(const Matrix &rhs);
 	virtual ~Matrix();
 
 	Matrix& operator=(const Matrix &rhs);
-	const Matrix& operator+=(const Matrix &rhs);
-	const Matrix& operator-=(const Matrix &rhs);
-	const Matrix& operator*=(double scalar);
+	Matrix& operator+=(const Matrix &rhs);
+	Matrix& operator-=(const Matrix &rhs);
+	Matrix& operator*=(const Matrix &rhs);
+	Matrix& operator*=(double scalar);
 
-	const Matrix operator+(const Matrix &rhs) const;
-	const Matrix operator-(const Matrix &rhs) const;
-	const Matrix operator-() const;
-	const Matrix operator~(); // inverse
-	const Matrix operator*(const Matrix &rhs) const;
-	const Matrix operator*(double scalar) const;
+	Matrix operator+(const Matrix &rhs) const;
+	Matrix operator-(const Matrix &rhs) const;
+	Matrix operator-() const;
+	Matrix operator~() const; // inverse
+	Matrix operator*(const Matrix &rhs) const;
+	Matrix operator*(double scalar) const;
+	double& operator()(int i, int j);
 
-	bool  operator==(const Matrix &other);
-	bool  operator!=(const Matrix &other);
+	const double& get(int i, int j) const;
+	double& set(int i, int j, double v=0.0);
+	int index(int i, int j) const;
 
-	const Matrix inverse(bool selfmodify=false);
-	const Matrix determinant();
-	double trace();
-	const Matrix transpose();
+	bool  operator==(const Matrix &other) const;
+	bool  operator!=(const Matrix &other) const;
+
+	void inverse();
+	double trace() const;
+	void transpose();
 
 	double* data;
 	int m;
 	int n;
+	bool external_data;
+	bool transposed;
 };
 
 #endif /* MATRIX_H_ */
